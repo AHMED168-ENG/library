a6cb5c10b16880c6964a0669968e39030c00a94f
ahmed reda <zaza090977777@gmail.com>
2021-08-24 16:04:29 +0200

    frist update

--- js/book_details.js
+++ js/book_details.js
@@ -1,3 +1,24 @@
+/* -------------------------- read more -------------------------------------*/
+var read_more = document.querySelectorAll("red_more");
+var all_text = document.querySelectorAll(".all_text");
+var read_more = document.querySelectorAll(".read_more");
+var arr = [false , false] ;
+read_more.forEach((element , key) => {
+    element.onclick = function() {
+        console.log("dd")
+        arr[key] = !arr[key]
+        if(arr[key]) {
+            read_more[key].innerHTML = "اقرا القليل";
+            all_text[key].style.display = "inline";
+        } else {
+            read_more[key].innerHTML = "اقرا المزيد";
+            all_text[key].style.display = "none";
+        }
+    }
+}); 
+
+/* -------------------------- read more -------------------------------------*/
+
 /* -------------------------- about small nave -------------------------------------*/
 var sm_nav_button = document.getElementById("sm_nav_btn");
 var dropDawn = document.getElementById("dropDawn");
